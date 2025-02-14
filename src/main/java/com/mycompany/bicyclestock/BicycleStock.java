cce5d77e336598917cf8189453b91b0d9f00ba0a
Camelo24 <hooddeugoue11@gmail.com>
2025-02-14 17:35:53 +0100

    Add files via upload

---
+++ src/main/java/com/mycompany/bicyclestock/BicycleStock.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bicyclestock;
+import views.LoginPage;
+
+/**
+ *
+ * @author kl
+ */
+public class BicycleStock {
+
+    public static void main(String[] args) {
+    System.out.println("Hello World!");
+    LoginPage Lp = new LoginPage();
+    Lp.setVisible(true);
+    }
+    
+}
